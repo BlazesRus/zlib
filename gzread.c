1ca93be0926b97af197c2ba48b84f12a744e8072
BlazesRus (James) <BlazesRus@yahoo.com>
2022-05-06 18:21:34 -0400

    Apply replacements for depreciated functions as fix for Compiler Warning (level 3) C4996

--- gzread.c
+++ gzread.c
@@ -28,7 +28,7 @@ local int gz_load(gz_statep state, unsigned char* buf, unsigned len, unsigned* h
         get = len - *have;
         if (get > max)
             get = max;
-        ret = read(state->fd, buf + *have, get);
+        ret = _read(state->fd, buf + *have, get);
         if (ret <= 0)
             break;
         *have += (unsigned)ret;
@@ -622,7 +622,7 @@ int ZEXPORT gzclose_r(file)
     err = state->err == Z_BUF_ERROR ? Z_BUF_ERROR : Z_OK;
     gz_error(state, Z_OK, NULL);
     free(state->path);
-    ret = close(state->fd);
+    ret = _close(state->fd);
     free(state);
     return ret ? Z_ERRNO : err;
 }
